536588290221064a6c0fa43de492aab94f6c7b07
MarinaM7 <marinamuntenita@gmail.com>
2023-01-11 18:45:08 +0100

    ok

--- Giorno3/src/Esercizio3/Es3.java
+++ Giorno3/src/Esercizio3/Es3.java
@@ -7,37 +7,36 @@ public class Es3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		Scanner myObj = new Scanner(System.in);
-//		System.out.println("Enter a string: ");
-//		String string = myObj.next();
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String string = myObj.nextLine();
+		
+		myObj.close();
+		
+		String esc = ":q";
+		int i = 0;
+		
+		while(!string.equals(esc) && i<string.length()) {
+			System.out.print(string.charAt(i) + ", ");
+            i++;
+        }
+	}
 //		
-//		myObj.close();
-//		
-//		while(string != ":q") {
-//			for (int i = 0; i < string.length(); i++) {
-//                System.out.println(string.charAt(i));
-//                if (i < string.length() - 1) {
-//                    System.out.println(", ");
+//		Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.print("Inserisci una stringa (per interrompere scrivi ':q'): ");
+//            String input = scanner.nextLine();
+//            if (input.equals(":q")) {
+//            	System.out.print("Esercizio chiuso");
+//                break;
+//            }
+//            for (int i = 0; i < input.length(); i++) {
+//                System.out.print(input.charAt(i));
+//                if (i < input.length() - 1) {
+//                    System.out.print(", ");
 //                }
 //            }
-//		}
-		
-		Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Inserisci una stringa (per interrompere scrivi ':q'): ");
-            String input = scanner.nextLine();
-            if (input.equals(":q")) {
-            	System.out.print("Esercizio chiuso");
-                break;
-            }
-            for (int i = 0; i < input.length(); i++) {
-                System.out.print(input.charAt(i));
-                if (i < input.length() - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-        }
-        scanner.close();
+//            System.out.println();
+//        }
+//        scanner.close();
 	}
-}
